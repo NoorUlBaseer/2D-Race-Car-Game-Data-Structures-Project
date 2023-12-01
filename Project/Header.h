5e0baf9ab184a7fb61022f7d208b33d5fba07609
Noor Ul Baseer <baseersoomro2013@gmail.com>
2023-12-02 02:38:21 +0500

    Grid created through graph.

--- Project/Header.h
+++ Project/Header.h
@@ -1,8 +1,289 @@
 #pragma once
 #include <iostream>
+#include <string>
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+//#define WHITE   "\033[37m"
+
 using namespace std;
 
-class Car {
-	string name;
+class Graph_Node {
+public:
+	string data;
+	float weight;
+	int x, y;
+	Graph_Node* next;
+
+	Graph_Node() {
+		data = "";
+		weight = 0;
+		x = 0;
+		y = 0;
+		next = NULL;
+	}
+
+	Graph_Node(string data, float weight, int x, int y) {
+		this->data = data;
+		this->weight = weight;
+		this->x = x;
+		this->y = y;
+		next = NULL;
+	}
 };
 
+class Linked_List_Node {
+public:
+	Graph_Node data;
+	Linked_List_Node* next;
+
+	Linked_List_Node(Graph_Node data) {
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+class Linked_List {
+private:
+	Linked_List_Node* head;
+
+public:
+	Linked_List() {
+		this->head = NULL;
+	}
+
+	void set_head(Linked_List_Node* head) {
+		this->head = head;
+	}
+
+	Linked_List_Node* get_head() {
+		return this->head;
+	}
+
+	void insert(Graph_Node data) {
+		Linked_List_Node* newNode = new Linked_List_Node(data);
+
+		if (this->head == nullptr) {
+			this->head = newNode;
+		}
+
+		else {
+			Linked_List_Node* temp = this->head;
+
+			while (temp->next) {
+				temp = temp->next;
+			}
+			temp->next = newNode;
+		}
+	}
+
+	void display() {
+		Linked_List_Node* temp = this->head;
+
+		while (temp) {
+			cout << temp->data.data << " ";
+			temp = temp->next;
+		}
+		cout << endl;
+	}
+};
+
+class Graph {
+private:
+	Linked_List* adjacency_list;
+	int num_of_vertices, num_of_vertex;
+
+public:
+	Graph(int num_of_vertices) {
+		this->num_of_vertices = num_of_vertices;
+		num_of_vertex = 0;
+		adjacency_list = new Linked_List[num_of_vertices];
+	}
+
+	void add_vertex(string vertex, int x, int y) {
+		Graph_Node* newNode = new Graph_Node(vertex, 0, x, y);
+		adjacency_list[num_of_vertex].insert(*newNode);
+		num_of_vertex++;
+	}
+
+	void add_edge(string vertex1, string vertex2, float weight) {
+		int index1 = -1, index2 = -1;
+
+		for (int i = 0; i < num_of_vertex; i++) {
+			if (adjacency_list[i].get_head()->data.data == vertex1) {
+				index1 = i;
+			}
+			if (adjacency_list[i].get_head()->data.data == vertex2) {
+				index2 = i;
+			}
+		}
+
+		if (index1 == -1 || index2 == -1) {
+			cout << "Vertex not found" << endl;
+			return;
+		}
+
+		Graph_Node* newNode1 = new Graph_Node(vertex2, weight, adjacency_list[index1].get_head()->data.x, adjacency_list[index1].get_head()->data.y);
+		Graph_Node* newNode2 = new Graph_Node(vertex1, weight, adjacency_list[index2].get_head()->data.x, adjacency_list[index2].get_head()->data.y);
+
+		adjacency_list[index1].insert(*newNode1);
+		adjacency_list[index2].insert(*newNode2);
+	}
+
+	void display() {
+		for (int i = 0; i < num_of_vertex; i++) {
+			adjacency_list[i].display();
+		}
+	}
+
+	void update_vertex(int x, int y, string new_vertex) {
+		// x and y being the x and y coordinates
+		for (int i = 0; i < num_of_vertex; i++) {
+			if (adjacency_list[i].get_head()->data.x == x && adjacency_list[i].get_head()->data.y == y) {
+				adjacency_list[i].get_head()->data.data = new_vertex;
+				break;
+			}
+		}
+	}
+
+	//get_vertex_cordinates_x("C")
+	int get_vertex_cordinates_x(string vertex) {
+		for (int i = 0; i < num_of_vertex; i++) {
+			if (adjacency_list[i].get_head()->data.data == vertex) {
+				return adjacency_list[i].get_head()->data.x;
+			}
+		}
+		return -1;
+	}
+
+	//get_vertex_cordinates_y("C")
+	int get_vertex_cordinates_y(string vertex) {
+		for (int i = 0; i < num_of_vertex; i++) {
+			if (adjacency_list[i].get_head()->data.data == vertex) {
+				return adjacency_list[i].get_head()->data.y;
+			}
+		}
+		return -1;
+	}
+
+	void display_grid() {
+		// Display grid in a 10x10 grid
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				int x = i;
+				int y = j;
+				Linked_List_Node* temp = adjacency_list[y * 10 + x].get_head();
+
+				if (temp == NULL) {
+					cout << "  ";
+				}
+				else {
+					//cout << temp->data.data << "  ";
+					if (temp->data.data == "C") {
+						cout << BLUE << temp->data.data << RESET << "   ";
+					}
+					else if (temp->data.data == "S" || temp->data.data == "E") {
+						cout << RED << temp->data.data << RESET << "   ";
+					}
+					else if (temp->data.data == "#") {
+						cout << YELLOW << temp->data.data << RESET << "   ";
+					}
+					else if (temp->data.data == "X") {
+						cout << GREEN << temp->data.data << RESET << "   ";
+					}
+					else {
+						cout << MAGENTA << temp->data.data << RESET << "   ";
+					}
+				}
+
+				// Check for right neighbor
+				if (j != 9) {
+					string right = to_string(x) + to_string(y + 1); // Convert int to string for right neighbor vertex
+					temp = adjacency_list[x * 10 + y].get_head()->next;
+
+					while (temp) {
+						if (temp->data.data == right) {
+							cout << "  ";
+							break;
+						}
+						temp = temp->next;
+					}
+				}
+			}
+
+			cout << endl;
+
+			// Check for bottom neighbor
+			if (i != 9) {
+				for (int j = 0; j < 10; j++) {
+					string bottom = to_string(i + 1) + to_string(j); // Convert int to string for bottom neighbor vertex
+					Linked_List_Node* temp = adjacency_list[i * 10 + j].get_head();
+
+					if (temp == NULL) {
+						cout << " ";
+					}
+					else {
+						while (temp) {
+							if (temp->data.data == bottom) {
+								cout << " ";
+								break;
+							}
+							temp = temp->next;
+						}
+					}
+					cout << "  ";
+				}
+			}
+			cout << endl;
+		}
+	}
+
+	void move(char input) {
+		int C_x = get_vertex_cordinates_x("C");
+		//cout << C_x << endl;
+		int C_y = get_vertex_cordinates_y("C");
+		//cout << C_y << endl;
+
+		if (input == 'w' || input == 72) { // 72 is up arrow
+			if (C_y == 0) {
+				cout << "Can't move up" << endl;
+			}
+			else {
+				update_vertex(C_x, C_y, "*");
+				update_vertex(C_x, C_y - 1, "C");
+			}
+		}
+		else if (input == 's' || input == 80) {
+			if (C_y == 9) {
+				cout << "Can't move down" << endl;
+			}
+			else {
+				update_vertex(C_x, C_y, "*");
+				update_vertex(C_x, C_y + 1, "C");
+			}
+		}
+		else if (input == 'd' || input == 77) {
+			if (C_x == 9) {
+				cout << "Can't move left" << endl;
+			}
+			else {
+				update_vertex(C_x, C_y, "*");
+				update_vertex(C_x + 1, C_y, "C");
+			}
+		}
+		else if (input == 'a' || input == 75) {
+			if (C_x == 0) {
+				cout << "Can't move right" << endl;
+			}
+			else {
+				update_vertex(C_x, C_y, "*");
+				update_vertex(C_x - 1, C_y, "C");
+			}
+		}
+	}
+};
--- Project/Project.cpp
+++ Project/Project.cpp
@@ -1,10 +1,75 @@
 #include <iostream>
+#include <string>
+#include <windows.h>
+#include <conio.h>
+#include "Header.h"
 using namespace std;
 
 int main() {
-	cout << "Hello World!" << endl;
-	cout << "Saleh entered";
-	cout << "Noor entered";
-	cout << "Noor entered again";
-	return 0;
+    Graph graph(100);
+    char move = ' ';
+
+    for (char i = 0; i < 10; i++) {
+        for (char j = 0; j < 10; j++) {
+            string vertex = string(1, 'A' + i) + to_string(j);
+            graph.add_vertex(vertex, i, j);
+        }
+    }
+
+    for (char i = 0; i < 10; i++) {
+        for (char j = 0; j < 10; j++) {
+            string vertex = string(1, 'A' + i) + to_string(j);
+            if (i > 0) {
+                string up = string(1, 'A' + i - 1) + to_string(j);
+                graph.add_edge(vertex, up, 1.0f);
+            }
+            if (i < 9) {
+                string down = string(1, 'A' + i + 1) + to_string(j);
+                graph.add_edge(vertex, down, 1.0f);
+            }
+            if (j > 0) {
+                string left = string(1, 'A' + i) + to_string(j - 1);
+                graph.add_edge(vertex, left, 1.0f);
+            }
+            if (j < 9) {
+                string right = string(1, 'A' + i) + to_string(j + 1);
+                graph.add_edge(vertex, right, 1.0f);
+            }
+        }
+    }
+
+    //now update whole grid to store '*' in it
+    for (char i = 0; i < 10; i++) {
+        for (char j = 0; j < 10; j++) {
+            string vertex = string(1, 'A' + i) + to_string(j);
+            graph.update_vertex(i, j, "*");
+        }
+    }
+
+    //now placve 'C' at (2, 5)
+    graph.update_vertex(2, 5, "C");
+
+    graph.update_vertex(0, 0, "S");
+
+    graph.update_vertex(9, 9, "E");
+
+    graph.update_vertex(5, 8, "X");
+
+    graph.update_vertex(3, 2, "#");
+
+    while (true) {
+		graph.display_grid();
+        
+        char move = _getch();
+
+		if (move == 27) { //ESC
+			break;
+		}
+		else {
+            system("cls");
+            graph.move(move);
+		}
+	}
+
+    return 0;
 }
